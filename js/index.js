a4f1145275cf57ed7cb30c4ad476f2e420c66c44
Jeremy <jeremylimjw@gmail.com>
2020-12-26 20:38:47 +0800

    update

--- js/index.js
+++ js/index.js
@@ -3,25 +3,40 @@ for (let i = 0; i < items.length; i++) {
     '<div class="card" data-name="' + items[i].name + '" data-src="' + items[i].src + '" data-thumb="' + items[i].thumb + '" data-stats="' + items[i].stats + '" data-co="' + items[i].co + '" data-aw="' + items[i].aw + '" data-status="' + items[i].status + '">' +
         '<div class="image">' +
             '<img height="180" width="180" src="' + items[i].thumb + '">';
+
     if (items[i].status == "SOLD") {
         template += '<span class="sold-overlay">SOLD</span>';
     } else if (items[i].status == "RESERVED") {
         template += '<span class="reserved-overlay">RESERVED</span>';
     }
+
     template += '</div>' +
         '<div class="title">' +
             '<span title="' + items[i].name + '"><b>' + items[i].name + '</b></span>' +
-        '</div>' +
+        '</div>' + 
         '<div class="text">' +
-            '<span style="color: #808080;">' + items[i].stats + '</span>' +
+        '<span style="color: #808080;">' + items[i].stats + '</span>';
+
+    if (items[i].aw != 0) {
+        template += 
             '<br>' +
             '<span>c/o ' + items[i].co + '</span>' +
             '<br>' +
             '<span>a/w ' + items[i].aw + '</span>' +
+        '</div>';
+    } else {
+        template += 
             '<br>' +
-            '<p class="sub-text">Click to view item</p>' +
-        '</div>' +
-    '</div>';
+            '<span>&nbsp;</span>' +
+            '<br>' +
+            '<span>&nbsp;</span>' +
+        '</div>';
+    }
+
+    template += '<br>' +
+        '<p class="sub-text">Click to view item</p>' + 
+        '</div>';
+
     document.getElementById("body").innerHTML += template;
 }
 
